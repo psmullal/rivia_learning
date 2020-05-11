509243e2c5d5fbbabc12a0ae01ae371bd84e629d
Administrator <shushs@gmail.com>
2020-05-10 21:23:09 -0400

    Messing with different cursor options

--- generator_iterator.py
+++ generator_iterator.py
@@ -11,9 +11,11 @@ def thinker():
     # think_lib = ' .oO*Oo. '
     # think_lib = ' -=#=- '
     # think_lib = '*#+-.'
-    think_lib = ['m!   ', 'om!  ', 'oom! ', 'boom!', 'Boom!', 'BOom!', 'BOOm!',
+    ''' think_lib = ['m!   ', 'om!  ', 'oom! ', 'boom!', 'Boom!', 'BOom!', 'BOOm!',
                  'BOOM!', '  *  ', ' <*> ', '<(*)>', '(***)', '** **', '*   *',
                  '     ']
+    '''
+    think_lib = '><((^>'
     while True:
         for i in think_lib:
             yield i
